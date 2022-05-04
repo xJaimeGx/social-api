dc85b15b03f03bbf7d3ecf7cfe4baafad75f10dc
xJaimeGx <beanieman019@protonmail.com>
2022-05-03 20:42:33 -0500

    modify thought and user routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -10,23 +10,26 @@ const {
   deleteReply
 } = require('../../controllers/thought-controller');
 
+//GET all and POST at /api/thoughts
 router
   .route('/')
   .get(getThoughts)
   .post(createNewThought);
 
+//GET one, PUT, and DELETE at /api/thoughts/:id
 router
   .route('/:id')
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
 
+//Post at /api/thoughts/:thoughtId/replies
 router
-  .route('/:thoughtId/reactions')
+  .route('/:thoughtId/replies')
   .post(createReply);
 
   router
-  .route('/:thoughtId/reactions/:reactionId')
+  .route('/:thoughtId/replies/:replyId')
   .delete(deleteReply);
 
 module.exports = router;
